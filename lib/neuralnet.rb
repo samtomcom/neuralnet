9682f50c254df22a47fc2c88b801e11cf0edb565
samtomcom <sam.comer@gmail.com>
2017-12-29 16:01:50 +0000

    removed dependecy on size given

--- lib/neuralnet.rb
+++ lib/neuralnet.rb
@@ -2,17 +2,18 @@ require 'neuralnet/version'
 require 'matrix'
 
 class NeuralNet
-  def initialize size, weights, bias
-    @bias = bias
-    @size = size
-    @weights = weights
+  def initialize genes
+    @weights, @biases = genes
+    @size = []
+
+    @biases.each { |layer| @size << layer.size }
   end
 
   # do MATRIX(inputs)*MATRIX(weights), optional bias, activation function
   def output inputs
     (0...(@size.size - 1)).each do |layer|
       inputs = (Matrix.row_vector(inputs) * Matrix.rows(@weights[layer])).to_a[0]
-      inputs = activate(bias(inputs, layer))
+      inputs = bias(inputs, layer)
     end
     return inputs
   end
@@ -30,7 +31,7 @@ class NeuralNet
 
   private
   def bias inputs, layer
-    return inputs.map! { |x| @bias ? (x+1) / (@size[layer]+1) : x / @size[layer] }
+    return inputs.map! { |x| ( x ) / @size[layer] }
   end
 
   #apply an activation function
