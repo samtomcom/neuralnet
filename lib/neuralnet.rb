f54dc20e04a4bfde503ac714486d9f5fd614c6e8
samtomcom <sam.comer@gmail.com>
2017-12-29 16:16:15 +0000

    removed linear mapping (was just flatten)

--- lib/neuralnet.rb
+++ lib/neuralnet.rb
@@ -4,29 +4,23 @@ require 'matrix'
 class NeuralNet
   def initialize genes
     @weights, @biases = genes
-    @size = []
 
-    @biases.each { |layer| @size << layer.size }
+    # determine size of network
+    @size = []
+    @weights.each { |layer| @size << layer.size }
+    @size << @biases[-1].size
   end
 
   # do MATRIX(inputs)*MATRIX(weights), optional bias, activation function
   def output inputs
     (0...(@size.size - 1)).each do |layer|
+      puts "#{inputs}, #{layer}"
       inputs = (Matrix.row_vector(inputs) * Matrix.rows(@weights[layer])).to_a[0]
+      puts "#{inputs}, #{layer}"
       inputs = bias(inputs, layer)
+      puts "#{inputs}, #{layer}"
     end
     return inputs
-  end
-
-  # convert the weights into a linear (1d) array for general purpose manipulation
-  def linear_weights_out
-    linear = []
-    @weights.each { |layer| layer.each { |node| node.each { |val| linear << val } } }
-    return linear
-  end
-
-  # convert linear array into the weights as a 3d array
-  def linear_weights_in
   end
 
   private
