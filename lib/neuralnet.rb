1e06b012c40aaf508b423d60f0188e010f576eb5
samtomcom <sam.comer@gmail.com>
2017-12-29 16:37:08 +0000

    working bias in genes

--- lib/neuralnet.rb
+++ lib/neuralnet.rb
@@ -14,22 +14,20 @@ class NeuralNet
   # do MATRIX(inputs)*MATRIX(weights), optional bias, activation function
   def output inputs
     (0...(@size.size - 1)).each do |layer|
-      puts "#{inputs}, #{layer}"
       inputs = (Matrix.row_vector(inputs) * Matrix.rows(@weights[layer])).to_a[0]
-      puts "#{inputs}, #{layer}"
-      inputs = bias(inputs, layer)
-      puts "#{inputs}, #{layer}"
+      inputs = activate(bias(inputs, layer))
     end
     return inputs
   end
 
   private
+  # apply the bias to the sum
   def bias inputs, layer
-    return inputs.map! { |x| ( x ) / @size[layer] }
+    return inputs.each_with_index.map { |x,i| x + @biases[layer][i] }
   end
 
-  #apply an activation function
+  # apply an activation function
   def activate matrix
-    return matrix.map! { |x| 0.5 * Math.tanh(x) + 0.5 }
+    return matrix.map! { |x| x }
   end
 end
